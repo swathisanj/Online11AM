0558fb546376f8b4f4ac5349662fcc8540f5ce96
swathisanj <swathiramh1995@gmail.com>
2022-04-04 14:15:27 +0200

    new code added

--- src/sample/practice.java
+++ src/sample/practice.java
@@ -210,7 +210,7 @@ driver.get("https://automationexplore.com/selenium-automation-practice-page/");
 	System.out.println("New branch created");
 	
 	
-	System.out.println("Conflict branch code");
+	System.out.println("New branch code");
 	System.out.println("Swathi branch code");
 	
 	
